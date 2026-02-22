7b75acbb3382cd2a2290188ea81a97df89412ea9
sanjulamukherjee-cloud <sanjulamukherjee@gmail.com>
2026-02-22 18:31:16 +0530

    Update OopsBanner.java

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,6 +1,13 @@
-public class OopsBanner{
-    public static void main(String[] args){
-        // uc1:
-        System.out.println("OOPS");
+class Test
+{
+    public static void main(String args[])
+    {
+System.out.println("*** *** *** ***");
+System.out.println("* * * * * * *  ");
+System.out.println("* * * * * * *  ");
+System.out.println("* * * * *** ***");
+System.out.println("* * * * *     *");
+System.out.println("* * * * *     *");
+System.out.println("*** *** *   ***");
     }
 }
